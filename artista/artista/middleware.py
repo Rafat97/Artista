abb2d7ff0c0b3f1135ec83d56a85a67193d23b96
farazkabir <45370774+farazkabir@users.noreply.github.com>
2020-09-21 11:31:32 +0600

    Front end all main layouts

--- artista/artista/middleware.py
+++ artista/artista/middleware.py
@@ -72,8 +72,9 @@ class CustomAuthMiddleware:
             user_uuid = request.session['user']
             user = User.objects.filter(uuid__exact=user_uuid)
             if not user:
-                response = redirect('logout_user')
-                return response
+               # response = redirect('logout_user')
+               # return response
+                redirect('/dashboard')
             else:
                 for url in self.USER_LOGIN_REDIRECT_NAME:
                     if url == resolve( request.path_info ).url_name :
